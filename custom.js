421cf347b3ab5f754b60dcfcf99558d0d8692be6
Md.Rezwanur Rahman <rezwanureee@gmail.com>
2024-10-24 18:30:27 +0600

    add/ menu, hero, category

---
+++ custom.js
@@ -0,0 +1,23 @@
+// for hero slider
+
+var swiper = new Swiper(".hero-sliders", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  speed: 1000,
+  //   autoplay: {
+  //     delay: 3000,
+  //   },
+});
+
+// for category slider
+
+var swiper = new Swiper(".category-items", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  speed: 1000,
+  // autoplay: {
+  //   delay: 3000,
+  // },
+});
